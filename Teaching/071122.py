5f8c5c37cbce3a161e159fd468ba6731d13eb630
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-11-07 11:11:26 +0200

    Create 071122.py

---
+++ Teaching/071122.py
@@ -0,0 +1,31 @@
+names = ["moti","sharon","dani","tomer"]
+# for i in names:
+#     print(i,end=" ")
+
+# print(names[1])
+
+# print(names[1][2])
+
+# print(names[0]+names[1])
+# length = len(names)
+# print(length)
+# lengt1=len(names[1])
+# print(lengt1)
+
+# for i in names:
+#     if i=="dani":
+#         print("dr")
+#     else:
+#         print("no")
+
+def one(x):
+    print(x)
+
+def two(y):
+    print(y)
+r=one(1)+two(2)   
+print(r)
+def main():    
+    one(3),two(5)
+    print(one(3)+two(4))
+main()
