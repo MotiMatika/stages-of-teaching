bf714b0b3a4234813bdc32068661e08e50fda216
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2024-09-05 20:22:40 +0300

    Create while.py
    while loop

---
+++ Teaching/while.py
@@ -0,0 +1,28 @@
+# x=10
+# while x<=10:
+#     print ("yes")
+
+# x=5
+# while x<=8:
+#     print ("yes")
+#     x=x+1
+
+# x=5
+# while x<=8:
+#     print (x)
+#     x=x+1
+
+# x=2
+# while x<=40:
+#     print (x)
+#     x=x+2
+
+ 
+# x=1
+# while x<=8:
+#     print (x*"*")
+#     x=x+1
+# x=8
+# while x>=1:
+#     print (x*"*")
+#     x=x-1
