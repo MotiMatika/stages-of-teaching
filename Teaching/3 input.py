2c0f2450b3e88e9fe6ddd48776f9f7c463414a73
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2024-09-14 12:55:59 +0300

    Update 3 input.py

--- Teaching/3 input.py
+++ Teaching/3 input.py
@@ -1,8 +1,8 @@
 #name = input("My Name : ")                      #למשתנה 'ניים' מוכנס הקלט
 #print("hi ",name, " what's going on ?")         #פלט בשילוב השם שהוכנס
 
-number = input("please enter a number  :  ")     # ההודעה -קלט של מספר
-print("haribua :",int(number)**2)                #פלט- הריבוע שלו
+# number = input("please enter a number  :  ")     # ההודעה -קלט של מספר
+# print("haribua :",int(number)**2)                #פלט- הריבוע שלו
 
 #x = 1              # תא איקס ששוה 1
 #y = 2              #תא 2 שווה 2
@@ -21,3 +21,26 @@ print("haribua :",int(number)**2)                #פלט- הריבוע שלו
 #print("The Second Number You Enterd Was :",x)   #ההודעה מציגה את המספרים בסדר הפוך להכנסתם
 #print("The First  Number You Enterd Was :",y)   #ההודעה מציגה את המספרים בסדר הפוך להכנסתם
                                      
+#הקוד מחשב כמות ומחיר של מוצרים ומחשב את עלות וכמות הפריטים
+# p_rice = float(input("\nwhat is the rice's price ?      "))
+# q_rice = int(input("what is the quntity of rice ?   "))
+# t_rice=q_rice*p_rice
+# p_bread = float(input("what is the bread's price ?     "))
+# q_bread =  int(input("what is the quntity of bread ?  "))
+# t_bread=q_bread*p_bread
+# p_milk = float(input("what is the milk's price ?      "))
+# q_milk = int(input("what is the quntity of milk ?   "))
+# t_milk=q_milk*p_milk
+# p_tomato = float(input("what is the tomato's price ?    "))
+# q_tomato = int(input("what is the quntity of tomato ? "))
+# t_tomato=q_tomato*p_tomato
+# total_price=t_rice+t_bread+t_milk+t_tomato
+# total_products=q_rice+q_bread+q_milk+q_tomato
+# print("\nyou bought :")
+# print(q_rice,"kg of rice.    total price is",q_rice,  "x",p_rice,  '=',t_rice)
+# print(q_bread,"breads.        total price is",q_bread, "x",p_bread, '=',t_bread)
+# print(q_milk,"milk.          total price is",q_milk,  "x",p_milk,  '=',t_milk)
+# print(q_tomato,"kg of tomato.  total price is",q_tomato,"x",p_tomato,'=',t_tomato)
+# print("total price -------------------------------->",total_price,"shekels\n")
+# print("quantity of items -------------------------->",total_products," items\n")
+
