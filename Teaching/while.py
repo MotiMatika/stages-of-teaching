33354175e0993454375bf78365c40917702b9c26
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2024-09-14 13:14:30 +0300

    Update while.py

--- Teaching/while.py
+++ Teaching/while.py
@@ -1,23 +1,27 @@
+#לולאה אינסופית
 # x=10
 # while x<=10:
 #     print ("yes")
 
+#כותב 4 פעמים את המילה 
 # x=5
 # while x<=8:
 #     print ("yes")
 #     x=x+1
 
+#כותב את המספרים מ 5 עד 8
 # x=5
 # while x<=8:
 #     print (x)
 #     x=x+1
 
+#כפולות 2 עד 40
 # x=2
 # while x<=40:
 #     print (x)
 #     x=x+2
 
- 
+#סידרה עולה ויורדת של כוכבים 
 # x=1
 # while x<=8:
 #     print (x*"*")
@@ -26,3 +30,21 @@
 # while x>=1:
 #     print (x*"*")
 #     x=x-1
+
+#הקוד-משחק מבקש הקלדה של מספר הקטן ממספר כלשהו
+# num=10
+# mistake=0
+# print("\nenter a number smaller than 10")
+# user_num=int(input("\nyour number is --->  "))
+
+# while user_num >= num:
+#     print ("\nyou are wrong\n")
+#     print("you already have ",mistake+1,"mistakes")
+#     mistake=mistake+1
+#     print("enter a number smaller than 10")
+#     user_num=int(input("\nyour number is --->  "))
+
+# print ("\nyou are wright\n")
+# print("you had",mistake,"mistakes\n")
+
+
